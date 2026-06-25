5d611be50f70da5817cdcebb73c42407c3e51607
JosephWilson <jwilson@staudertech.com>
2026-06-25 15:47:14 -0500

    add component.html and single point of change for header/footer

--- app-config.js
+++ app-config.js
@@ -1,1 +1,22 @@
 // app-config.js
+
+// Load navbar and footer from components.html
+fetch("../components.html")
+  .then(res => res.text())
+  .then(html => {
+
+    // Create a temporary DOM for reading
+    const temp = document.createElement("div");
+    temp.innerHTML = html;
+
+    // Inject navbar
+    const navbar = temp.querySelector(".nav");
+    document.getElementById("navbar").appendChild(navbar);
+
+    // Inject footer
+    const footer = temp.querySelector(".footer");
+    document.getElementById("footer").appendChild(footer);
+  });
+
+
+  // I NEED TO GO THROUGH THIS
